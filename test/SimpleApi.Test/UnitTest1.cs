dc79175f3b20d677ad626fc4d5e18873ef8f31ee
Lawrence Ong <lawrenceong@hotmail.com>
2021-07-13 15:58:04 -0400

    Initial Commit

---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+using SimpleApi.Controllers;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+
+    [Fact]
+    public void GetReturnsAnyValue()
+    {
+      var serviceProvider = new ServiceCollection()
+          .AddLogging()
+          .BuildServiceProvider();
+
+      var factory = serviceProvider.GetService<ILoggerFactory>();
+
+      var logger = factory.CreateLogger<WeatherForecastController>();
+
+      WeatherForecastController controller = new WeatherForecastController(logger);
+
+      var forecasts = controller.Get();
+      var count = ((WeatherForecast[])forecasts).Length;
+      Assert.Equal(5,count);
+    }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
